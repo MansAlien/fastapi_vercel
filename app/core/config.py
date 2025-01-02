28f13e52de98acbac74069d156b9581fc2c5e530
MansAlien <mans012033945421@gmail.com>
2025-01-02 15:00:37 +0200

    ignore the data/

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///./test.db"
+    DATABASE_URL: str = "sqlite:///./data/test.db"
     SECRET_KEY: str = "your-secret-key"
     DEBUG: bool = True
 
